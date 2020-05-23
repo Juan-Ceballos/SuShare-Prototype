b8e6a876591fc30aee0da3e6a6560e7f61775b7b
Juan C <juanceballos@pursuit.org>
2020-05-22 21:10:51 -0400

    fc

---
+++ SuShare-Prototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuShare-Prototype
+//
+//  Created by Juan Ceballos on 5/22/20.
+//  Copyright © 2020 Juan Ceballos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+    }
+
+
+}
+
